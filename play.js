0520216ebcbe4c84bfbcae78f575e483d68cf488
Mussie <musdzion@gmail.com>
2024-03-19 15:55:00 -0400

    play client code

---
+++ play.js
@@ -0,0 +1,36 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host:'localhost', // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  //new codes
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  conn.on("connect", () => {
+    conn.write("Hello from client!");
+  });
+ 
+
+  return conn;
+};
+
+console.log("Connecting ...");
+
+
+connect();
+
+
+
+
+
+
