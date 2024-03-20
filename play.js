f919123afeef004ff4ab58b4ec39957cbb2257bb
Mussie <musdzion@gmail.com>
2024-03-20 15:31:48 -0400

    exit controls added

--- play.js
+++ play.js
@@ -4,6 +4,30 @@ const connect = require("./client")  //import the connect function
 console.log("Connecting ...");
 connect(); //function connect is used
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+
+
+const handleUserInput = function(key) {
+
+  // \u0003 maps to ctrl+c input
+if (key === '\u0003') {
+  process.exit();
+}
+};
+
+
+setupInput();
 
 
 
